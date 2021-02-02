37a066d5ff95e5c6390aab16640c0768f98346d3
Viacheslav Tomin <cithreepio@gmail.com>
2021-02-02 19:26:30 +0200

    adds modal menu dropout

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function burgerMenu(selector) {
   let menu = $(selector);
   let button = menu.find('.menu-button,.buy-now');
@@ -18,3 +19,19 @@ function burgerMenu(selector) {
       }
   }
 }
+=======
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
+    menuBtnRef.addEventListener("click", () => {
+      const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+  
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  })();
+>>>>>>> Stashed changes
